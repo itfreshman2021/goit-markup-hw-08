e9e37afe320a4b95ed8bfd0816aeee5576a4928e
Ruslan Filonchuk <itfreshman@ukr.net>
2021-08-24 15:10:31 +0300

    Додано розмітку та стилі для Mobile First
    Додано розмітку та стилі для Mobile First

--- js/modal.js
+++ js/modal.js
@@ -8,7 +8,8 @@
         refs.openModalBtn.addEventListener('click', toggleModal);
         refs.closeModalBtn.addEventListener('click', toggleModal);
 
-        function toggleModal() {
-            refs.modal.classList.toggle('backdrop--is-hidden');
+    function toggleModal() {
+        document.body.classList.toggle('modal-open');
+        refs.modal.classList.toggle('backdrop--is-hidden');
         }
     })();
